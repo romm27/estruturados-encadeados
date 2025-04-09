86a221abeef4ac74af187acb41d4b967b24d0b9f
Giovanni Strasser <giostrasser@gmail.com>
2025-04-08 21:07:19 -0300

    Implementa estrutura do slide

---
+++ app/src/main/java/com/example/estruturados/encadeados/ListaEncadeada.java
@@ -0,0 +1,9 @@
+package com.example.estruturados.encadeados;
+
+class ListaEncadeada {
+    public Node lista;
+    
+    public ListaEncadeada(){
+        lista = null;
+    }
+}
---
+++ app/src/main/java/com/example/estruturados/encadeados/Node.java
@@ -0,0 +1,11 @@
+package com.example.estruturados.encadeados;
+
+public class Node {
+    public Integer dado;
+    public Node proximo;
+    
+    public Node(){
+        dado = null;
+        proximo = null;
+    }
+}
